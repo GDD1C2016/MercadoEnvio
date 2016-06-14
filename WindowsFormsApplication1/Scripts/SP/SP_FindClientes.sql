0938bc3f23c22e3e2296cdcb0b72b58301e70961
tamiberrocal <tamaraberrocal@gmail.com>
2016-06-13 22:02:39 -0300

    Actualización de scripts

--- WindowsFormsApplication1/Scripts/SP/SP_FindClientes.sql
+++ WindowsFormsApplication1/Scripts/SP/SP_FindClientes.sql
@@ -12,21 +12,20 @@ GO
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_FindClientes] 
 	@FiltroNombre nvarchar(255) = '', 
-	@FiltroApellido nvarchar(255) = '',
-	@FiltroEmail nvarchar(255) = '',
+	@FiltroApellido nvarchar(255) = '', 
+	@FiltroEmail nvarchar(255) = '', 
 	@FiltroDni numeric(18,0)
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT C.[IdUsuario] AS IdUsuario, C.[Nombre] AS Nombre, C.[Apellido] AS Apellido, C.[Calle] AS Calle, C.[Nro] AS NroCalle, C.[CP] AS CodigoPostal, C.[Departamento] AS Departamento,
-	C.[Mail] AS Email, C.[FechaNacimiento] AS FechaNacimiento, C.[Localidad] AS Localidad, C.[NroDoc] AS NroDoc, C.[Piso] AS Piso, C.[Telefono] AS Telefono, C.[TipoDoc] AS TipoDoc,
-	U.[Activo] AS Activo, U.[UserName] AS UserName, U.[PassEncr] AS PasswordEnc
+	SELECT C.[IdUsuario] AS IdUsuario, C.[Nombre] AS Nombre, C.[Apellido] AS Apellido, C.[Calle] AS Calle, C.[Nro] AS NroCalle, C.[CP] AS CodigoPostal, C.[Departamento] AS Departamento, C.[Mail] AS Email, C.[FechaNacimiento] AS FechaNacimiento, C.[Localidad] AS Localidad, C.[NroDoc] AS NroDoc, C.[Piso] AS Piso, C.[Telefono] AS Telefono, C.[TipoDoc] AS TipoDoc, U.[Activo] AS Activo, U.[UserName] AS UserName, U.[PassEncr] AS PasswordEnc
 	FROM [GD1C2016].[MASTERDBA].[Clientes] C, [GD1C2016].[MASTERDBA].[Usuarios] U
-	WHERE C.[IdUsuario] = U.[IdUsuario] 
+	WHERE C.[IdUsuario] = U.[IdUsuario]
 	AND (@FiltroNombre = '' OR C.[Nombre] LIKE @FiltroNombre + '%')
 	AND (@FiltroApellido = '' OR C.[Apellido] LIKE @FiltroApellido + '%')
 	AND (@FiltroEmail = '' OR C.[Mail] LIKE @FiltroEmail + '%')
 	AND (@FiltroDni = 0 OR C.[NroDoc] = @FiltroDni)
 END
 
+GO
--- WindowsFormsApplication1/Scripts/SP/SP_FindEmpresas.sql
+++ WindowsFormsApplication1/Scripts/SP/SP_FindEmpresas.sql
@@ -12,19 +12,18 @@ GO
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_FindEmpresas] 
 	@FiltroRazonSocial nvarchar(255) = '', 
-	@FiltroCuit nvarchar(50) = '',
+	@FiltroCuit nvarchar(50) = '', 
 	@FiltroEmail nvarchar(255) = ''
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT E.[IdUsuario] AS IdUsuario, E.[RazonSocial] AS RazonSocial, E.[Ciudad] AS Ciudad, E.[Calle] AS Calle, E.[Nro] AS NroCalle, E.[CP] AS CodigoPostal, E.[Departamento] AS Departamento,
-	E.[Mail] AS Email, E.[Rubro] AS Rubro, E.[Localidad] AS Localidad, E.[CUIT] AS Cuit, E.[Piso] AS Piso, E.[Telefono] AS Telefono, E.[Contacto] AS Contacto,
-	U.[Activo] AS Activo, U.[UserName] AS UserName, U.[PassEncr] AS PasswordEnc
+	SELECT E.[IdUsuario] AS IdUsuario, E.[RazonSocial] AS RazonSocial, E.[Ciudad] AS Ciudad, E.[Calle] AS Calle, E.[Nro] AS NroCalle, E.[CP] AS CodigoPostal, E.[Departamento] AS Departamento, E.[Mail] AS Email, E.[Rubro] AS Rubro, E.[Localidad] AS Localidad, E.[CUIT] AS Cuit, E.[Piso] AS Piso, E.[Telefono] AS Telefono, E.[Contacto] AS Contacto, U.[Activo] AS Activo, U.[UserName] AS UserName, U.[PassEncr] AS PasswordEnc
 	FROM [GD1C2016].[MASTERDBA].[Empresas] E, [GD1C2016].[MASTERDBA].[Usuarios] U
-	WHERE E.[IdUsuario] = U.[IdUsuario] 
+	WHERE E.[IdUsuario] = U.[IdUsuario]
 	AND (@FiltroRazonSocial = '' OR E.[RazonSocial] LIKE @FiltroRazonSocial + '%')
 	AND (@FiltroCuit = '' OR E.[CUIT] LIKE @FiltroCuit + '%')
 	AND (@FiltroEmail = '' OR E.[Mail] LIKE @FiltroEmail + '%')
 END
 
+GO
