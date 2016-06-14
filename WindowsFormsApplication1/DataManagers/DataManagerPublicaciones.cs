f1ddc590f20e60f0bdf60ecbc943495cc48810bc
tamiberrocal <tamaraberrocal@gmail.com>
2016-06-14 00:05:57 -0300

    Publicación

--- WindowsFormsApplication1/DataManagers/DataManagerPublicaciones.cs
+++ WindowsFormsApplication1/DataManagers/DataManagerPublicaciones.cs
@@ -4,6 +4,7 @@ using MercadoEnvio.Entidades;
 using MercadoEnvio.Helpers;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace MercadoEnvio.DataManagers
 {
@@ -78,52 +79,62 @@ namespace MercadoEnvio.DataManagers
                 db.EndConnection();
 
                 return publicaciones;
-        }
+            }
         }
 
-        private static List<Publicacion> FindPublicaciones(string filtroDescripcion, List<Rubro> rubrosFiltro, DataBaseHelper db)
+        private static List<Publicacion> FindPublicaciones(string filtroDescripcion, List<Rubro> rubrosFiltro,
+            DataBaseHelper db)
         {
             List<Publicacion> publicaciones = new List<Publicacion>();
-            //List<SqlParameter> parameters = new List<SqlParameter>();
-            //DataTable res =
+            List<SqlParameter> parameters = new List<SqlParameter>();
 
-            //foreach (Rubro rubro in rubrosFiltro)
-            //{
-            //    SqlParameter filtroDescripcionParameter = new SqlParameter("@FiltroDescripcion", SqlDbType.NVarChar);
-            //    filtroDescripcionParameter.Value = filtroDescripcion.Trim();
+            foreach (Rubro rubro in rubrosFiltro)
+            {
+                SqlParameter filtroDescripcionParameter = new SqlParameter("@FiltroDescripcion", SqlDbType.NVarChar);
+                filtroDescripcionParameter.Value = filtroDescripcion.Trim();
 
-            //    SqlParameter filtroIdRubroParameter = new SqlParameter("@FiltroIdRubro", SqlDbType.Int);
-            //    filtroIdRubroParameter.Value = rubro.IdRubro;
+                SqlParameter filtroIdRubroParameter = new SqlParameter("@FiltroIdRubro", SqlDbType.Int);
+                filtroIdRubroParameter.Value = rubro.IdRubro;
 
-            //    parameters.Add(filtroDescripcionParameter);
-            //    parameters.Add(filtroIdRubroParameter);
+                parameters.Add(filtroDescripcionParameter);
+                parameters.Add(filtroIdRubroParameter);
 
-            //    DataTable res = db.GetDataAsTable("SP_FindPublicaciones", parameters);
-            //    foreach (DataRow row in res.Rows)
-            //    {
-            //        var publicacion = new Publicacion
-            //        {
-            //            IdPublicacion = Convert.ToInt32(row["IdPublicacion"]),
-            //            Descripcion = Convert.ToString(row["Descripcion"]),
-            //            Stock = Convert.ToInt32(row["Stock"]),
-            //            FechaInicio = Convert.ToDateTime(row["FechaInicio"]),
-            //            FechaVencimiento = Convert.ToDateTime(row["FechaVencimiento"]),
-            //            Precio = Convert.ToDecimal(row["Precio"]),
-            //            PrecioReserva = Convert.ToDecimal(row["Precio"]),
-            //            IdRubro = Convert.ToInt32(row["IdRubro"]),
-            //            IdUsuario = Convert.ToInt32(row["IdUsuario"]),
-            //            IdEstado = Convert.ToInt32(row["IdEstado"]),
-            //            Envio = Convert.ToBoolean(row["Envio"]),
-            //        };
+                DataTable res = db.GetDataAsTable("SP_FindPublicaciones", parameters);
+                foreach (DataRow row in res.Rows)
+                {
+                    var publicacion = new Publicacion();
 
-            //        publicacion.Visibilidad = GetVisibilidadPublicacion(publicacion.IdPublicacion, db);
-            //        publicacion.TipoPublicacion = GetTipoPublicacion(publicacion.IdPublicacion, db);
+                    publicacion.IdPublicacion = Convert.ToInt32(row["IdPublicacion"]);
+                    publicacion.Descripcion = Convert.ToString(row["Descripcion"]);
+                    publicacion.Stock = Convert.ToInt32(row["Stock"]);
+                    publicacion.FechaInicio = Convert.ToDateTime(row["FechaInicio"]);
+                    publicacion.FechaVencimiento = Convert.ToDateTime(row["FechaVencimiento"]);
+                    publicacion.Precio = Convert.ToDecimal(row["Precio"]);
+                    publicacion.PrecioReserva = Convert.ToDecimal(row["Precio"]);
+                    publicacion.IdRubro = Convert.ToInt32(row["IdRubro"]);
+                    publicacion.IdUsuario = Convert.ToInt32(row["IdUsuario"]);
+                    publicacion.IdEstado = Convert.ToInt32(row["IdEstado"]);
+                    publicacion.Envio = Convert.ToBoolean(row["Envio"]);
+                    publicacion.Visibilidad = new Visibilidad
+                    {
+                        IdVisibilidad = Convert.ToInt32(row["IdVisibilidad"]),
+                        Descripcion = Convert.ToString(row["DescripcionVisibilidad"]),
+                        Precio = Convert.ToDecimal(row["Precio"]),
+                        Porcentaje = Convert.ToDecimal(row["Porcentaje"]),
+                        EnvioPorcentaje = Convert.ToDecimal(row["EnvioPorcentaje"])
+                    };
+                    publicacion.TipoPublicacion = new TipoPublicacion
+                    {
+                        IdTipo = Convert.ToInt32(row["IdTipo"]),
+                        Descripcion = Convert.ToString(row["DescripcionTipoPublicacion"]),
+                        Envio = Convert.ToBoolean(row["Envio"]),
+                    };
 
-            //        publicaciones.Add(publicacion);
+                    publicaciones.Add(publicacion);
 
-            //        parameters.Clear();
-            //    }
-            //}
+                    parameters.Clear();
+                }
+            }
 
             return publicaciones;
         }
