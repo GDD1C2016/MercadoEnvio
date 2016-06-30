3e48480dd2e0f3839eaff90bd77c63c5563e0593
tamiberrocal <tamaraberrocal@gmail.com>
2016-06-30 01:10:27 -0300

    .

--- WindowsFormsApplication1/Entidades/TipoPublicacion.cs
+++ WindowsFormsApplication1/Entidades/TipoPublicacion.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
-
-namespace MercadoEnvio.Entidades
+﻿namespace MercadoEnvio.Entidades
 {
     public class TipoPublicacion
     {
