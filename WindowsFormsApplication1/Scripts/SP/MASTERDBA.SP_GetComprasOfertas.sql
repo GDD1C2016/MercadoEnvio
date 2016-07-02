b1de4dcb1d8a6afd5d75429c7dc35f5956be52c5
tamiberrocal <tamaraberrocal@gmail.com>
2016-07-02 00:05:07 -0300

    .

---
+++ WindowsFormsApplication1/Scripts/SP/MASTERDBA.SP_GetComprasOfertas.sql
@@ -0,0 +1,59 @@
+﻿USE [GD1C2016]
+GO
+/****** Object:  StoredProcedure [MASTERDBA].[SP_GetComprasOfertas]    Script Date: 02/07/2016 12:01:01 a.m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [MASTERDBA].[SP_GetComprasOfertas] 
+	@IdUsuario int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+		C.[IdCompra] AS Id,
+		C.[IdPublicacion],
+		C.[Fecha],
+		C.[Cantidad],
+		C.[IdUsuario],
+		P.[TipoDescripcion] AS TipoPublicacion,
+		C.[Descripcion] AS DescripcionPublicacion,
+		C.[NombreUsuario] AS Vendedor,
+		NULL AS Adjudicada,
+		P.[Precio]
+	FROM
+		[GD1C2016].[MASTERDBA].[VW_Compras] C,
+		[GD1C2016].[MASTERDBA].[VW_Publicaciones] P
+	WHERE C.[IdPublicacion] = P.[IdPublicacion]
+	AND C.[IdRubro] = P.[IdRubro]
+	AND P.[VisibilidadActual] = 1
+	AND C.[IdUsuario] = @IdUsuario
+	AND C.[IdCompra] NOT IN (SELECT O.[IdCompra] FROM [GD1C2016].[MASTERDBA].[Ofertas] O WHERE O.[IdCompra] IS NOT NULL)
+	UNION ALL
+	SELECT
+		O.[IdOferta] AS Id,
+		O.[IdPublicacion],
+		O.[Fecha],
+		1 AS Cantidad,
+		O.[IdUsuario],
+		P.[TipoDescripcion] AS TipoPublicacion,
+		O.[Descripcion] AS DescripcionPublicacion,
+		O.[NombreUsuario] AS Vendedor,
+		NULL AS Adjudicada,
+		O.[Monto] AS Precio
+	FROM
+		[GD1C2016].[MASTERDBA].[VW_Ofertas] O,
+		[GD1C2016].[MASTERDBA].[VW_Publicaciones] P
+	WHERE O.[IdPublicacion] = P.[IdPublicacion]
+	AND O.[IdRubro] = P.[IdRubro]
+	AND P.[VisibilidadActual] = 1
+	AND O.[IdUsuario] = @IdUsuario
+END
+
+GO
